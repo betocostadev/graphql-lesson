0be520044c9559a0444488a30e2b00fa1542dc10
betocostadev <robertomarx@gmail.com>
2019-10-11 09:31:05 -0300

    added apollo client, grapql, apollo bost. Changed collections overview to use apollo

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+// Using Apollo instead of Redux (Also using a HOC - collections overview-container)
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
-
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import './collections-overview.styles.scss';
 
@@ -16,8 +13,4 @@ const CollectionsOverview = ({ collections }) => (
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollectionsForPreview
-});
-
-export default connect(mapStateToProps)(CollectionsOverview);
+export default CollectionsOverview;
